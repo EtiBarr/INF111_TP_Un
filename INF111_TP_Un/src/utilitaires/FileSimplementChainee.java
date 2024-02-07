6f64a8ada41ecf9c472dd81f1ec8874cc9cb4714
EtiBarr <etienne.barriere.1@ens.etsmtl.ca>
2024-02-07 08:20:00 -0500

    file simplement chaine in progress

--- INF111_TP_Un/src/utilitaires/FileSimplementChainee.java
+++ INF111_TP_Un/src/utilitaires/FileSimplementChainee.java
@@ -1,4 +1,34 @@
 package utilitaires;
 
 public class FileSimplementChainee {
+
+    int cle;
+    FileSimplementChainee next;
+
+   
+        public FileSimplementChainee(int cle) { //constructueur pour cree une nouvelle fille
+
+            this.cle = cle;
+            this.next = null;
+        }
+
+
+        void enQueue(int cle) {
+
+            // Create a new LL node
+            FileSimplementChainee temp = new FileSimplementChainee(cle);
+
+            // If queue is empty, then new node is front and
+            // rear both
+            if (this.rear == null) {
+                this.front = this.rear = temp;
+                return;
+            }
+
+            // Add the new node at the end of queue and change
+            // rear
+            this.rear.next = temp;
+            this.rear = temp;
+        }
+    }
 }
